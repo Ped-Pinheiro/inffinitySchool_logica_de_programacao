4e4ce71f984c8d06ba47188a940fd168a3760356
Ped-Pinheiro <henlion99@gmail.com>
2023-03-06 20:44:38 -0300

    adding questions 4,5,6

---
+++ question_five.py
@@ -0,0 +1,7 @@
+#Make a code to convert meters for centimeters.
+
+number_meters = int(input('Digit a number to converter meters for contimeters: '))
+
+number_centimenters = number_meters * 100
+
+print(f'The number in centimenters is: {number_centimenters}cm')
