ee619170e9dbed2d4e1f983d713898261a89b42c
Ped-Pinheiro <henlion99@gmail.com>
2023-03-07 21:00:56 -0300

    question 6 done

--- question_six.py
+++ question_six.py
@@ -1,1 +1,8 @@
-#
+#Make a code to get a radius of the circle, calculate and show the area.
+
+radius = float(input('Digit a radius: '))
+
+area = 3.14*radius**2
+
+print(f'The area of circle is: {int(area)}')
+
