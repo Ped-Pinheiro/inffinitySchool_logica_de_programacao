cc3770fc6009d74fe824fbeccaeeb7482fe84aeb
Ped-Pinheiro <henlion99@gmail.com>
2023-03-08 21:06:50 -0300

    question seven and eight done

---
+++ question_eight.py
@@ -0,0 +1,7 @@
+
+money_per_hour = float(input('Digit the money per hour you make from the work: '))
+hours_worked = int(input('Digit how many hours you work of a mouth: '))
+
+total = money_per_hour*hours_worked
+
+print(f'The total of salary you gain on a mouth is: {round(total,2)}')
--- question_seven.py
+++ question_seven.py
@@ -1,2 +1,7 @@
-#
+#Make a code to calculate an area of square, so shows the double. 
 
+square_area = int(input('Digit a number: '))
+
+total = square_area**2
+
+print(f'The total of area is: {total*2}')
