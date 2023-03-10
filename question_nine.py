977e5a6912ee3324e23bc17cba087938b0ce8b8b
Ped-Pinheiro <henlion99@gmail.com>
2023-03-10 05:56:46 -0300

    questions nine and ten done

---
+++ question_nine.py
@@ -0,0 +1,6 @@
+
+fahrenheit = float(input('Digit a number in Fahrenheit: '))
+
+celsius = 5*((fahrenheit-32) / 9)
+
+print(f'The temperature in celsius is: {round(celsius,2)}')
---
+++ question_ten.py
@@ -0,0 +1,7 @@
+
+
+celsius = float(input('Digit a number celsius temperature: '))
+
+fahrenheit = celsius * 1.8 + 32
+
+print(f'The temperature in Fahrenheit is: {round(fahrenheit,2)}')
